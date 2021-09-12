476b5116aa233a9c512b103377edea619d1ea766
ArpitSureka <surekaarpit@gmail.com>
2021-09-12 13:27:36 +0530

    changes

--- Blog_website/urls.py
+++ Blog_website/urls.py
@@ -20,7 +20,7 @@ from users import views as user_views
 
 urlpatterns = [
     path('', include("blog.urls")),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name="control-admin"),
     path('login/', auth_views.LoginView.as_view(template_name="users/login.html"), name='login'),
     path('register/', user_views.register, name='register'),
     path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.html"), name='logout'),
